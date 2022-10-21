dd32c22ef0dac71d863ba89a7646c7a2c82bf204
arturasvell <arturasvell@gmail.com>
2022-10-21 12:45:05 +0300

    Some changes

--- AudioVisualizationWPF/MainWindow.xaml.cs
+++ AudioVisualizationWPF/MainWindow.xaml.cs
@@ -16,9 +16,199 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Accord.Math;
+using System.Drawing;
+using Color = System.Drawing.Color;
+using Point = System.Windows.Point;
+using SoundPlayer = System.Media.SoundPlayer;
+using NAudio.Wave;
 
 namespace AudioVisualizationWPF
 {
+    public class WAVFile
+    {
+        public double duration { get; set; }
+        public int chunkID { get; set; }
+        public int fileSize { get; set; }
+        public int riffType { get; set; }
+        public int fmtID { get; set; }
+        public int fmtSize { get; set; }
+        public int fmtCode { get; set; }
+        public int channels { get; set; }
+        public int sampleRate { get; set; }
+        public int byteRate { get; set; }
+        public int fmtBlockAlign { get; set; }
+        public int bitDepth { get; set; }
+        public int fmtExtraSize { get; set; }
+        public int dataID { get; set; }
+
+        public int bytes { get; set; }
+        public byte[] byteArray { get; set; }
+        public int byteLength { get; set; }
+        public double[]? leftChannel { get; set; }
+        public double[]? rightChannel { get; set; }
+        public bool Read(string filename)
+        {
+            sampleRate = 0;
+            byteLength = 0;
+            byteRate = 0;
+            leftChannel = null;
+            rightChannel = null;
+            try
+            {
+                using (FileStream fs = File.Open(filename, FileMode.Open))
+                {
+                    BinaryReader reader = new BinaryReader(fs);
+
+                    // chunk 0
+                    chunkID = reader.ReadInt32();
+                    fileSize = reader.ReadInt32();
+                    riffType = reader.ReadInt32();
+                    byteLength += 12;
+
+                    // chunk 1
+                    fmtID = reader.ReadInt32();
+                    fmtSize = reader.ReadInt32(); // bytes for this chunk (expect 16 or 18)
+                    byteLength += 8;
+
+                    // 16 bytes.
+                    fmtCode = reader.ReadInt16();
+                    channels = reader.ReadInt16();
+                    sampleRate = reader.ReadInt32();
+                    byteRate = reader.ReadInt32();
+                    fmtBlockAlign = reader.ReadInt16();
+                    bitDepth = reader.ReadInt16();
+                    byteLength += 16;
+
+                    if (fmtSize == 18)
+                    {
+                        fmtExtraSize = reader.ReadInt16();
+                        reader.ReadBytes(fmtExtraSize);
+                        byteLength += fmtExtraSize;
+                    }
+
+                    // chunk 2
+                    dataID = reader.ReadInt32();
+                    bytes = reader.ReadInt32();
+                    byteLength += 8 + bytes;
+                    duration = (byteLength - 8) / byteRate;
+                    // data
+                    byteArray = reader.ReadBytes(bytes);
+
+                    int bytesForSamp = bitDepth / 8;
+                    int nValues = bytes / bytesForSamp;
+
+
+                    double[] asDouble = null;
+                    switch (bitDepth)
+                    {
+                        case 64:
+                            asDouble = new double[nValues];
+                            Buffer.BlockCopy(byteArray, 0, asDouble, 0, bytes);
+                            asDouble = Array.ConvertAll(asDouble, e => (double)e);
+                            break;
+                        case 32:
+                            asDouble = new double[nValues];
+                            Buffer.BlockCopy(byteArray, 0, asDouble, 0, bytes);
+                            break;
+                        case 16:
+                            Int16[]
+                                asInt16 = new Int16[nValues];
+                            Buffer.BlockCopy(byteArray, 0, asInt16, 0, bytes);
+                            asDouble = Array.ConvertAll(asInt16, e => e / (double)(Int16.MaxValue + 1));
+                            break;
+                        default:
+                            return false;
+                    }
+
+                    switch (channels)
+                    {
+                        case 1:
+                            leftChannel = asDouble;
+                            rightChannel = null;
+                            return true;
+                        case 2:
+                            // de-interleave
+                            int nSamps = nValues / 2;
+                            leftChannel = new double[nSamps];
+                            rightChannel = new double[nSamps];
+                            for (int s = 0, v = 0; s < nSamps; s++)
+                            {
+                                leftChannel[s] = asDouble[v++];
+                                rightChannel[s] = asDouble[v++];
+                            }
+                            return true;
+                        default:
+                            return false;
+                    }
+                }
+            }
+            catch
+            {
+                Debug.WriteLine("Failed to load: " + filename);
+                return false;
+            }
+        }
+
+        public void AddFadeIn(double timeMarker)
+        {
+
+        }
+        public void CreateWAV(string filename)
+        {
+            try
+            {
+                int bytesForSamp = bitDepth / 8;
+                int nValues = bytes / bytesForSamp;
+                double[] combinedArray = new double[nValues];
+                switch (channels)
+                {
+                    case 1:
+                        combinedArray = leftChannel;
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        break;
+                }
+                byte[] toWrite = new byte[bytes];
+                switch (bitDepth)
+                {
+                    case 64:
+                        Buffer.BlockCopy(combinedArray, 0, toWrite, 0, bytes);
+                        break;
+                    case 32:
+                        Buffer.BlockCopy(combinedArray, 0, toWrite, 0, bytes);
+                        break;
+                    case 16:
+                        Int16[]
+                            asInt16 = new Int16[nValues];
+                        byte[] convertedBack = new byte[bytes];
+                        for (int i = 0; i < combinedArray.Length; i++)
+                        {
+                            combinedArray[i] *= (double)(Int16.MaxValue + 1);
+                            asInt16[i] = (Int16)combinedArray[i];
+                        }
+                        Buffer.BlockCopy(asInt16, 0, toWrite, 0, bytes);
+                        break;
+                    default:
+                        break;
+                }
+                WaveFormat waveFormat = new WaveFormat(sampleRate, channels);
+                using (WaveFileWriter writer = new WaveFileWriter("test.wav", waveFormat))
+                {
+                    writer.Write(toWrite, 0, bytes);
+                }
+                
+            }
+            catch
+            {
+                Debug.Fail("Failed to write file.");
+            }
+
+
+        }
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -29,6 +219,10 @@ namespace AudioVisualizationWPF
         private bool secondChannelExists = false;
         private double[] currentEnergy = null;
         private double[] currentZCR = null;
+        private SoundPlayer currentSoundPlayer = null;
+        private double[] currentL = null;
+        private double[] currentR = null;
+        private WAVFile? file = null;
         public MainWindow()
         {
             InitializeComponent();
@@ -152,6 +346,7 @@ namespace AudioVisualizationWPF
                     string filePath = ofd.FileName;
                     txtFilePath.Text = filePath;
                     audioFilePath = filePath;
+                    currentSoundPlayer = new SoundPlayer(filePath);
                 }
             }
             catch (Exception ex)
@@ -160,7 +355,7 @@ namespace AudioVisualizationWPF
             }
 
         }
-        private void PlotAudio(double[] left, double[] right, int duration)
+        private void PlotAudio(double[]? left, double[]? right, int duration)
         {
             try
             {
@@ -180,13 +375,13 @@ namespace AudioVisualizationWPF
                 {
                     secondChannelExists = false;
                 }
-                mainPlot.Plot.Title("WAV File Data - Right Channel");
+                mainPlot.Plot.Title("WAV File Data - Left Channel");
                 mainPlot.Plot.XLabel("Time (seconds)");
                 mainPlot.Plot.YLabel("Audio Value");
                 mainPlot.Plot.AxisAuto(0);
                 mainPlot.Plot.Benchmark(true);
 
-                mainPlot2.Plot.Title("WAV File Data - Left Channel");
+                mainPlot2.Plot.Title("WAV File Data - Right Channel");
                 mainPlot2.Plot.XLabel("Time (seconds)");
                 mainPlot2.Plot.YLabel("Audio Value");
                 mainPlot2.Plot.AxisAuto(0);
@@ -202,25 +397,30 @@ namespace AudioVisualizationWPF
             }
 
         }
-        private void btnPlotAudio_Click(object sender, RoutedEventArgs e)
+        private void PlotAudio()
         {
             try
             {
                 if (File.Exists(audioFilePath))
                 {
-                    double[] left;
-                    double[] right;
-                    int sampleRate;
-                    int byteLength;
-                    int byteRate;
-                    readWav(audioFilePath, out left, out right, out sampleRate, out byteLength, out byteRate);
-                    audioFileDuration = (byteLength - 8) / byteRate;
+                    file = new WAVFile();
+                    file.Read(audioFilePath);
+                    if (file.leftChannel != null)
+                    {
+                        currentL = file.leftChannel;
+                    }
+                    if (file.rightChannel != null)
+                    {
+                        currentR = file.rightChannel;
+                    }
+
+                    audioFileDuration = (file.byteLength - 8) / file.byteRate;
                     Debug.WriteLine("Duration is " + audioFileDuration.ToString());
-                    PlotAudio(left, right, sampleRate);
-                    ComputeFrames(left, audioFileDuration * 1000,15, out List<double> energy);
+                    PlotAudio(file.leftChannel, file.rightChannel, file.sampleRate);
+                    ComputeFrames(file.leftChannel, audioFileDuration * 1000, 15, out List<double> energy);
                     PlotEnergy(energy);
                     Debug.WriteLine(String.Format("Energy has {0} elements", energy.Count));
-                    ComputeZeroCrossRate(left, audioFileDuration * 1000, 15, out List<double> zeroCross);
+                    ComputeZeroCrossRate(file.leftChannel, audioFileDuration * 1000, 15, out List<double> zeroCross);
                     PlotZeroCrossRate(zeroCross);
                     Debug.WriteLine(String.Format("Zero Cross Rate has {0} elements", zeroCross.Count));
                 }
@@ -230,6 +430,10 @@ namespace AudioVisualizationWPF
                 Debug.WriteLine(ex);
                 MessageBox.Show(ex.Message);
             }
+        }
+        private void btnPlotAudio_Click(object sender, RoutedEventArgs e)
+        {
+            PlotAudio();
         }
 
         private void ComputeZeroCrossRate(double[] signal, double duration, int frameLength, out List<double> zeroCross)
@@ -516,29 +720,30 @@ namespace AudioVisualizationWPF
             var plottables = plot.Plot.GetPlottables();
             for (int i = 0; i < plottables.Length; i++)
             {
-                if (plottables[i].GetType() == typeof(ScottPlot.Plottable.MarkerPlot))
+                if (plottables[i].GetType() == typeof(ScottPlot.Plottable.ScatterPlot))
                 {
-                    plot.Plot.RemoveAt(i);
+                    plot.Plot.Remove(plottables[i]);
                 }
             }
+            plot.Render();
         }
-        private void AddMarkerToPlot(ScottPlot.WpfPlot plot, bool isHorizontal, double value)
+        private void AddMarkerToPlot(ScottPlot.WpfPlot plot, bool isHorizontal, double value, Color color)
         {
             
             if(isHorizontal)
             {
-                var marker = plot.Plot.AddLine(0, value, 1000, value, System.Drawing.Color.Red);
+                var marker = plot.Plot.AddLine(0, value, 1000, value, color);
             }
             else
             {
-                var marker = plot.Plot.AddLine(value, 0, value,1000, System.Drawing.Color.Red);
+                var marker = plot.Plot.AddLine(value, 0, value,1000, color);
             }
 
             plot.Render();
         }
         private void ConnectSegments(double value1, double value2, double minValue, ScottPlot.WpfPlot plot)
         {
-            var marker = plot.Plot.AddLine(value1, minValue, value2, minValue, System.Drawing.Color.Red);
+            var marker = plot.Plot.AddLine(value1, 0, value2, 0, System.Drawing.Color.Blue);
             plot.Render();
         }
         private void btnFindZCRSegments_Click(object sender, RoutedEventArgs e)
@@ -546,7 +751,7 @@ namespace AudioVisualizationWPF
             ClearPlottables(mainPlot4);
             if(double.TryParse(txtFindZCRSegments.Text, out double minValue))
             {
-                AddMarkerToPlot(mainPlot4, true, minValue);
+                AddMarkerToPlot(mainPlot4, true, minValue, Color.Red);
                 int segmentStart = 0;
                 int segmentEnd = 0;
                 bool started = false;
@@ -564,8 +769,8 @@ namespace AudioVisualizationWPF
                     {
                         segmentEnd = i;
                         started = false;
-                        AddMarkerToPlot(mainPlot4, false, segmentStart);
-                        AddMarkerToPlot(mainPlot4, false, segmentEnd);
+                        AddMarkerToPlot(mainPlot4, false, segmentStart, Color.Blue);
+                        AddMarkerToPlot(mainPlot4, false, segmentEnd, Color.Blue);
                         ConnectSegments(segmentStart, segmentEnd, minValue, mainPlot4);
                     }
                 }
@@ -577,7 +782,7 @@ namespace AudioVisualizationWPF
             ClearPlottables(mainPlot3);
             if (double.TryParse(txtEnergyMinValue.Text, out double minValue))
             {
-                AddMarkerToPlot(mainPlot3, true, minValue);
+                AddMarkerToPlot(mainPlot3, true, minValue, Color.Red);
                 int segmentStart = 0;
                 int segmentEnd = 0;
                 bool started = false;
@@ -595,12 +800,95 @@ namespace AudioVisualizationWPF
                     {
                         segmentEnd = i;
                         started = false;
-                        AddMarkerToPlot(mainPlot3, false, segmentStart);
-                        AddMarkerToPlot(mainPlot3, false, segmentEnd);
+                        AddMarkerToPlot(mainPlot3, false, segmentStart,Color.Blue);
+                        AddMarkerToPlot(mainPlot3, false, segmentEnd,Color.Blue);
                         ConnectSegments(segmentStart, segmentEnd, minValue, mainPlot3);
                     }
                 }
             }
+        }
+
+        private void btnPlayAudio_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (currentSoundPlayer == null)
+                {
+                    throw new Exception("Sound file not opened succesfully!");
+                }
+                if(currentL==null)
+                {
+                    throw new Exception("Left channel does not exist!");
+                }
+                file.CreateWAV("test.wav");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            
+        }
+
+        private void btnPlayAudioRight_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (currentSoundPlayer == null)
+                {
+                    throw new Exception("Sound file not opened succesfully!");
+                }
+                if (currentL == null)
+                {
+                    throw new Exception("Right channel does not exist!");
+                }
+                using (MemoryStream ms = new MemoryStream(currentR.ToByte()))
+                {
+                    SoundPlayer player = new SoundPlayer(ms);
+                    player.Play();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnAddEcho_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double s = double.Parse(txtEchoCoef.Text);
+                double timeMarker = double.Parse(txtDeltaDistance.Text);
+                double deltaD = (timeMarker/1000) / audioFileDuration * file.leftChannel.Length;
+                int delta = (int)deltaD;
+                delta = (int)(timeMarker / 1000 * file.sampleRate);
+                for (int i = 0; i < file.leftChannel.Length; i++)
+                {
+                    if(i-delta>=0)
+                    {
+                        file.leftChannel[i] = file.leftChannel[i] + file.leftChannel[i - delta] * s;
+                        if (file.rightChannel!=null)
+                        {
+                            file.rightChannel[i] = file.rightChannel[i] + file.rightChannel[i - delta] * s;
+                        }
+                        
+                    }
+                    
+                }
+                PlotAudio(file.leftChannel, file.rightChannel, file.sampleRate);
+                ComputeFrames(file.leftChannel, audioFileDuration * 1000, 15, out List<double> energy);
+                PlotEnergy(energy);
+                Debug.WriteLine(String.Format("Energy has {0} elements", energy.Count));
+                ComputeZeroCrossRate(file.leftChannel, audioFileDuration * 1000, 15, out List<double> zeroCross);
+                PlotZeroCrossRate(zeroCross);
+                Debug.WriteLine(String.Format("Zero Cross Rate has {0} elements", zeroCross.Count));
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
         }
     }
 }
